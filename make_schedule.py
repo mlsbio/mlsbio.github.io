180c8c518596ddc8b3c18ab7c7386f0e15d851a8
Roshan Rao <roshan_rao@berkeley.edu>
2021-11-09 12:50:32 -0800

    add schedule

---
+++ make_schedule.py
@@ -0,0 +1,64 @@
+from operator import methodcaller
+from typing import Optional, List
+from datetime import datetime, timedelta
+from itertools import cycle
+import csv
+
+
+class Schedule:
+    def __init__(self, path: str):
+        self.start = datetime(
+            year=2021,
+            month=12,
+            day=13,
+            hour=9,
+            minute=0,
+        )
+        self.currtime = self.start
+
+        colors = cycle(["stage-saturn", "stage-earth"])
+        self.blocks: List[List[str]] = []
+        with open(path, encoding="utf-8-sig") as f:
+            reader = csv.DictReader(map(methodcaller("strip"), f))
+            for line in reader:
+                if line["Title"].startswith("*"):
+                    self.blocks.append([])
+                else:
+                    self.make_event(
+                        line["Title"],
+                        line["Description"],
+                        int(line["Length"]),
+                        color=next(colors),
+                    )
+
+    def get_schedule(self) -> str:
+        data = '<div class="row">'
+        for block in self.blocks:
+            data += '<div class="col-md-6"><table class="schedule">'
+            data += "".join(block)
+            data += '</table></div>'
+        data += '</row>'
+        return data
+
+    def make_event(
+        self,
+        title: str,
+        description: Optional[str],
+        length: int,
+        color: str,
+    ):
+        rowspan = length // 10
+        first_block = (
+            f'<tr><th>{self.currtime.strftime("%I:%M")}</th><td colspan="4" '
+            f'rowspan={rowspan} class="{color}">{title}'
+        )
+        if description:
+            first_block += f'<p style="font-weight:normal">{description}</p>'
+        first_block += "</td></tr>"
+        self.blocks[-1].append(first_block)
+        self.currtime += timedelta(minutes=10)
+        for _ in range(1, rowspan):
+            self.blocks[-1].append(
+                f"<tr><th>{self.currtime.strftime('%I:%M')}</th></tr>"
+            )
+            self.currtime += timedelta(minutes=10)
