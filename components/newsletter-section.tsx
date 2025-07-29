720d5d5d40e4bfcbd55f3b4368a1a32bcfe4a2cc
Simon Duerr <dev@simonduerr.eu>
2025-07-29 14:15:53 +0200

    Update newsletter-section.tsx

--- components/newsletter-section.tsx
+++ components/newsletter-section.tsx
@@ -59,6 +59,7 @@ export default function NewsletterSection() {
           </div>
           <div className="border-t border-slate-700 mt-8 pt-8 text-center text-slate-400">
             <p>&copy; 2025 MLSB Workshop. All rights reserved.</p>
+            <p className="pt-4 text-xs">The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
           </div>
         </div>
       </footer>
